63f9c0d014e985b11df6ff11b66e0b3d1f9de265
ivan110kg <ivan110kg@gmail.com>
2021-10-19 17:49:21 +0400

    test commit

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import os
 def get_response(url):
     request = requests.get(url, headers={
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'})
-    return request
+    if request.status_code < 400:
+        return request
 
 
 def main():
