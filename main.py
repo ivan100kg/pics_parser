10e5fe6c3c4d716db1a3505fc9a649b819ed276d
ivar <ivar110kg@gmail.com>
2021-10-20 18:59:42 +0400

    add doc strings and timer

--- main.py
+++ main.py
@@ -4,45 +4,72 @@ from os.path import basename
 import requests
 from bs4 import BeautifulSoup
 import os
+from datetime import datetime
+
+
+def timer(foo):
+    """function-decorator"""
+    def wrapper(*args, **kwargs):
+        print(f'Start {foo.__name__}')
+        start = datetime.now()
+        result = foo(*args, **kwargs)
+        print(f'{foo.__name__} is finished - execution time: {datetime.now() - start}')
+        return result
+    return wrapper
 
 
 def get_response(url):
-    """get response"""
+    """This function returns response from url"""
     request = requests.get(url, headers={
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'})
     if request.status_code < 400:
         return request
+    else:
+        return None
 
 
-def main():
-    """main function"""
+async def async_func():
+    """asynchronous function"""
+    pass
+
+
+@timer
+def sync_func():
+    """synchronous function"""
+
+    # set variables
     url = 'https://ivash-ka.ru'
-
-    if not os.path.exists('images/'):
-        os.mkdir('images/')
-
-    request = get_response(url + '/catalog/zhenskoe/')
+    women_clothes = '/catalog/zhenskoe/'
+    pic_dir = 'images/'
+    request = get_response(url + women_clothes)
     soup = BeautifulSoup(request.text, 'lxml')
 
-    catalog_list = soup.find('div', {'class': 'catalog_list'})
-    tag_links = catalog_list.find_all('a', {'class': 'name'})
-    links = []
-    for link in tag_links:
-        links.append((link.text.strip(), url + link.get('href')))
+    # check dir
+    if not os.path.exists(pic_dir):
+        os.mkdir(pic_dir)
 
-    for link in links:
-        dir_name = link[0]
-        if not os.path.exists('images/' + dir_name):
-            os.mkdir('images/' + dir_name)
-        request = get_response(link[1])
+    # image search
+    tag_links = soup.find_all('a', {'class': 'name'})
+    data = []  # list of (category name, category url)
+    [data.append((tag_link.text.strip(), url + tag_link.get('href'))) for tag_link in tag_links]
+    # get name and url from data
+    for category_name, category_url in data:
+        dir_name = category_name
+        # check dirs
+        if not os.path.exists(pic_dir + dir_name):
+            os.mkdir(pic_dir + dir_name)
+        # getting responses from pages and search images
+        request = get_response(category_url)
         soup = BeautifulSoup(request.text, 'lxml')
-        catalog_list = soup.find('div', {'class': 'catalog_list'})
-        tag_pics = catalog_list.find_all('img')
+        # catalog_list = soup.find('div', {'class': 'catalog_list'})
+        # tag_pics = catalog_list.find_all('img', {'class': 'lazy'})
+        tag_pics = soup.find_all('img', {'class': 'lazy'})
+        # save images to storage
         for pic in tag_pics:
             src = url + pic.get('data-original')
-            with open(f'images/{dir_name}/{basename(src)}', 'wb') as f:
+            with open(f'{pic_dir}{dir_name}/{basename(src)}', 'wb') as f:
                 f.write(requests.get(src).content)
 
 
 if __name__ == '__main__':
-    main()
+    sync_func()
